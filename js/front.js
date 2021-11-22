17bb1af4dd87772944e49a8ad2e6e29bfd1afdd0
jcvisk <juan.carlos.gcn@gmail.com>
2021-11-22 08:42:31 -0600

    My First Commit

---
+++ js/front.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+    let navHeight = $('#header').height();
+    $('#separator').css("height", navHeight);
+
+    //Inicializando WOW
+    new WOW().init();
+
+    //Obteniedo el año actual para el copyright
+    /*
+    copyright = new Date();
+    update = copyright.getFullYear();
+    document.getElementById('year').innerHTML = update;
+    */
+
+    //Cambiando colores del navbar
+    $(window).scroll(function(){
+        if ($("#nav").offset().top > 56) {
+            //Cambiando color de navbar
+            $("#nav").removeClass("bg-transparent");
+            $("#nav").addClass("bg-glass");  
+
+            if ($('#header').hasClass("navbar-blue")) {
+                $("#header").removeClass('navbar-blue');
+                $("#header").addClass('flag');
+            }
+            
+        }else{
+            //Cambiando color de navbar
+            $("#nav").removeClass("bg-glass");
+            $("#nav").addClass("bg-transparent");
+
+            if ($('#header').hasClass("flag")) {
+                $("#header").addClass('navbar-blue');
+                $("#header").removeClass('flag');
+            }
+        }
+    });
+    
+    /*
+    Haciendo que el nav abarque toda la pantalla al desplegarse
+    */
+    $('#nav-toggler').click(function(){
+        $("#header").toggleClass('align-start');
+        
+        $("#nav").toggleClass('align-start');
+        $("#nav").toggleClass('bg-glass-toggle');
+    });
+});
+
+
+
+
+
+
+
+
+
